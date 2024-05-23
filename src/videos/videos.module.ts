b8bb9081ec558ac93dceee0da48abb51031d998f
Lautaro Fullone <lautarofullone@gmail.com>
2024-05-23 17:10:44 -0300

    video module fix

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { VideosService } from './videos.service';
 import { VideosController } from './videos.controller';
 import { CoursesModule } from 'src/courses/courses.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Video, VideoSchema } from './model/video.schema';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    MongooseModule.forFeature(
+      [
+        { name: Video.name, schema: VideoSchema }
+      ]
+    ),
+  ],
   controllers: [VideosController],
   providers: [VideosService],
 })
